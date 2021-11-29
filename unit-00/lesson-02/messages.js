7e21362ff9a2384235b3a0b1830926149dccaba5
Gonzalo FU <GFU-code@protonmail.ch>
2021-11-29 11:24:23 -0300

    unit00-lesson2: primer acercamiento con node js, revisando su REPL

---
+++ unit-00/lesson-02/messages.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const printMessages = (arr) => {
+  arr.forEach((message) => console.log(message));
+};
+
+let messages = [
+  "A change of environment can ba a good thing!",
+  "You will make it!",
+  "Just run with the code!",
+];
---
+++ unit-00/lesson-02/print.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// Formas de interpolar variables en una cadena de texto
+
+console.log("Hello %s", "Universe");
+console.log("Score: %d", 100);
+
+let x = "Universe";
+console.log(`Hello, ${x}`);
