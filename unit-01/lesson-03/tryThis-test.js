443dc94c144adeb9c614fd6e79ff246bd9e51b64
Gonzalo FU <GFU-code@protonmail.ch>
2021-11-30 12:30:23 -0300

    unit01-lesson3 - Practicando con el <Try This> final de la lección

---
+++ unit-01/lesson-03/tryThis-test.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const tryThis = require("./tryThis");
+
+console.log(tryThis.addNum(10, 34));
+console.log(tryThis.restNum(30, 47));
+console.log(tryThis.multNum(3, 7));
---
+++ unit-01/lesson-03/tryThis.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const addNum = (x, y) => x + y;
+const restNum = (x, y) => x - y;
+const multNum = (x, y) => x * y;
+
+module.exports = {
+  addNum,
+  restNum,
+  multNum,
+};
