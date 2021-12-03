233a87dd6a07f235eb2534835845e572b099d466
Gonzalo FU <GFU-code@protonmail.ch>
2021-12-02 21:04:53 -0300

    unit01-lesson05 - Analizando y manejando los datos entrantes en una solicitud POST

--- unit-01/lesson-05/second-server/main.js
+++ unit-01/lesson-05/second-server/main.js
@@ -6,6 +6,29 @@ const app = http.createServer();
 
 // Definimos un evento sobre el servidor
 app.on("request", (req, res) => {
+  // Analizando los datos entrantes
+  // Registra el método HTTP usado
+  console.log(`Method: ${getJSONString(req.method)}`);
+  // Registra la URL
+  console.log(`URL: ${getJSONString(req.url)}`);
+  // Registra la cabecera
+  console.log(`Headers: ${getJSONString(req.headers)}`);
+
+  // Manejando datos enviados por método POST
+  // Arreglo para manejar cada uno de los trozos que representan al contenido
+  let body = [];
+  // Configuramos un callback para ir registrando cada uno de los trozos de datos
+  // entrantes
+  req.on("data", (bodyData) => {
+    body.push(bodyData);
+  });
+  // Cuando la transmisión de datos finalice, ejecutamos el callback definido
+  req.on("end", () => {
+    // Convertimos el cuerpo de arreglo a una cadena de texto
+    body = Buffer.concat(body).toString();
+    console.log(`Request Body Contents: ${body}`);
+  });
+
   res.writeHead(httpStatus.StatusCodes.OK, { "Content-Type": "text/html" });
 
   let responseMessages = "<h1>This will show on the screen.</h1>";
@@ -14,3 +37,8 @@ app.on("request", (req, res) => {
 
 app.listen(port);
 console.log(`The server has started and is listening on port number ${port}`);
+
+// Funcionalidades
+
+// Convierte un objeto javascript en una cadena
+const getJSONString = (obj) => JSON.stringify(obj, null, 2);
