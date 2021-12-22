292beed729a11c771f8e5279394f6ef03879fedd
Gonzalo FU <GFU-code@protonmail.ch>
2021-12-21 23:57:15 -0300

    unit02-lesson10 - Intro a los views engine. Estableciendo un diseño base (layouts) y haciendo uso de partials para simplificar las partes compartidas entre los archivos de nuestras páginas webs.

--- unit-02/lesson-10/express_templates/main.js
+++ unit-02/lesson-10/express_templates/main.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const layouts = require("express-ejs-layouts");
 
 // Módulos propios
 const homeController = require("./controllers/homeController");
@@ -9,6 +10,11 @@ const app = express();
 app.set("port", process.env.PORT || 3000);
 // Express sabrá donde buscar nuestras vistas siempre y cuando usemos como motor de plantillas ejs, caso contrario, le deberemos indicar su ubicación.
 app.set("view engine", "ejs");
+
+// Middlewares
+// layouts le indica a express y ejs que cada vista que sea renderizada usara como plantilla de diseño una llamada layouts.
+// De esta manera podemos evitar la repetición de código HTML al hacer que varios archivos compartan cosas en común.
+app.use(layouts);
 
 app.get("/", homeController.welcomeClient);
 app.get("/name/:myName", homeController.respondWithName);
