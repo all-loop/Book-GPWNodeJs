68ef517d22fc3c221e35c679bc8a3b8c91eecfc9
Gonzalo FU <GFU-code@protonmail.ch>
2021-12-29 10:31:49 -0300

    unit04-lesson18 - Estructura base de la lección 18

---
+++ unit-04/lesson-18/user-model/models/subscriber.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+// Validando datos con mongoose
+const subscriberSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+    unique: true,
+  },
+  zipCode: {
+    type: Number,
+    min: [10000, "Zip code too short"],
+    max: 99999,
+  },
+  courses: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course",
+    },
+  ],
+});
+
+// Método de instancia para obtener el nombre completo de un subscriptor.
+subscriberSchema.methods.getInfo = function () {
+  return `Name: ${this.name} Email: ${this.email} Zip Code: ${this.zipCode}`;
+};
+
+// Método de instancia para encontrar todos los subscriptores con el mismo Zip Code
+subscriberSchema.methods.findLocalSubscribers = function () {
+  return this.model("Subscriber").find({ zipCode: this.zipCode }).exec();
+};
+
+module.exports = mongoose.model("subscriber", subscriberSchema);
