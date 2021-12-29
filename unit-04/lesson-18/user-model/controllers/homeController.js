62f218f13aa9c7b3faa28ef6e2e285bad1729a10
Gonzalo FU <GFU-code@protonmail.ch>
2021-12-29 11:53:24 -0300

    unit04-lesson18 - se incorporo la estructura base para comenzar a enlazar nuestras vistas junto a nuestros modelos y aplicar las operaciones CRUD esperadas para cada modelo

---
+++ unit-04/lesson-18/user-model/controllers/homeController.js
@@ -0,0 +1,34 @@
+const courses = [
+  {
+    title: "Event Driven Cakes",
+    cost: 50,
+  },
+  {
+    title: "Asynchronous Artichoke",
+    cost: 25,
+  },
+  {
+    title: "Object Oriented Orange Juice",
+    cost: 10,
+  },
+];
+
+const showCourses = (req, res) => {
+  res.render("courses", {
+    offeredCourses: courses,
+  });
+};
+
+const showSignUp = (req, res) => {
+  res.render("contact");
+};
+
+const postedSignUpForm = (req, res) => {
+  res.render("thanks");
+};
+
+module.exports = {
+  showCourses,
+  showSignUp,
+  postedSignUpForm,
+};
---
+++ unit-04/lesson-18/user-model/controllers/subscribersController.js
@@ -0,0 +1,50 @@
+const Subcriber = require("../models/subscriber");
+
+// Función que obtiene todos los subscriptores en la db
+const getAllSubscribers = (req, res) => {
+  Subcriber.find({})
+    .exec()
+    .then((subscribers) => {
+      res.render("subscribers", { subscribers });
+    })
+    .catch((error) => {
+      console.log(error.message);
+      return [];
+    })
+    .finally(() => {
+      console.log("promise <find all subscribers> complete!");
+    });
+};
+
+// Renderiza la página de contacto
+const getSubscriptionPage = (req, res) => {
+  res.render("contact");
+};
+
+// Función que guarda en db a un subscriptor
+const saveSubscriber = (req, res) => {
+  let newSubscriber = new Subcriber({
+    name: req.body.name,
+    email: req.body.email,
+    zipCode: req.body.zipCode,
+  });
+
+  newSubscriber
+    .save()
+    .then((result) => {
+      console.log(`successfully save subscriptor: ${result}`);
+      res.render("thanks");
+    })
+    .catch((error) => {
+      console.log(error.message), res.send(error);
+    })
+    .finally(() => {
+      console.log("Promise <save subscriber> complete!");
+    });
+};
+
+module.exports = {
+  getAllSubscribers,
+  getSubscriptionPage,
+  saveSubscriber,
+};
