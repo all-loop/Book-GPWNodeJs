ee27af1130f825564fe4490e202c3fcb842701f5
Gonzalo FU <GFU-code@protonmail.ch>
2022-01-11 00:23:18 -0300

    unit04-lesson21 - Se construyo cada una de las rutas necesarias para la aplicación. Quedo todo listo para continuar con el desarrollo de las acciones.

--- unit-04/lesson-21/confetti_cuisine/controllers/homeController.js
+++ unit-04/lesson-21/confetti_cuisine/controllers/homeController.js
@@ -1,34 +1,7 @@
-const courses = [
-  {
-    title: "Event Driven Cakes",
-    cost: 50,
-  },
-  {
-    title: "Asynchronous Artichoke",
-    cost: 25,
-  },
-  {
-    title: "Object Oriented Orange Juice",
-    cost: 10,
-  },
-];
-
-const showCourses = (req, res) => {
-  res.render("courses", {
-    offeredCourses: courses,
-  });
-};
-
-const showSignUp = (req, res) => {
-  res.render("contact");
-};
-
-const postedSignUpForm = (req, res) => {
-  res.render("thanks");
+const index = (req, res) => {
+  res.render("index");
 };
 
 module.exports = {
-  showCourses,
-  showSignUp,
-  postedSignUpForm,
+  index,
 };
--- unit-04/lesson-21/confetti_cuisine/main.js
+++ unit-04/lesson-21/confetti_cuisine/main.js
@@ -19,9 +19,13 @@ mongoose.connection.on("open", () => {
 const homeController = require("./controllers/homeController");
 const errorController = require("./controllers/errorController");
 const subscriberController = require("./controllers/subscribersController");
+const courseController = require("./controllers/coursesController");
+const userController = require("./controllers/usersController");
 
 // Creación del servidor
 const app = express();
+// Creamos un objeto router para manejar el enrutamiento de la applicación
+const router = express.Router();
 
 // Configuraciones del servidor
 app.set("port", process.env.PORT || 3000);
@@ -29,36 +33,105 @@ app.set("view engine", "ejs");
 
 // Middlewares del servidor
 // ---
+app.use("/", router);
 
 // middleware para usar el módulo layout en nuestras vistas
-app.use(layouts);
+router.use(layouts);
 
 // middleware que establece nuestra la carpeta pública al cliente
-app.use(express.static("public"));
+router.use(express.static("public"));
 
 // middlewares para interpretar las solicitudes entrantes.
 // Le indicamos a la aplicación que use body-parser para el
 // procesamiento de los parámetros codificados en la URL y JSON.
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+router.use(express.urlencoded({ extended: false }));
+router.use(express.json());
 
 // Configuramos la asistencia de métodos HTTP actualmente
 // no soportados por enlaces o formularios HTML
-app.use(
+router.use(
   methodOverride("_method", {
     methods: ["GET", "POST"],
   })
 );
 
-// Enturamiento
-app.get("/", (req, res) => {
-  res.render("index");
-});
-app.get("/courses", homeController.showCourses);
-app.get("/contact", subscriberController.getSubscriptionPage);
-app.get("/subscribers", subscriberController.getAllSubscribers);
-app.post("/subscribe", subscriberController.saveSubscriber);
-app.post("/contact", homeController.postedSignUpForm);
+// Enrutamiento
+router.get("/", homeController.index);
+
+// Rutas del subscriptor
+router.get(
+  "/subscribers",
+  subscriberController.index,
+  subscriberController.indexView
+);
+router.get("/subscribers/new", subscriberController.newSubscriber);
+router.post(
+  "/subscribers/create",
+  subscriberController.create,
+  subscriberController.redirectView
+);
+router.get(
+  "/subscribers/get/:id",
+  subscriberController.show,
+  subscriberController.showView
+);
+router.get("/subscribers/:id/edit", subscriberController.edit);
+router.put(
+  "/subscribers/:id/update",
+  subscriberController.update,
+  subscriberController.redirectView
+);
+router.delete(
+  "/subscribers/:id/delete",
+  subscriberController.erased,
+  subscriberController.redirectView
+);
+
+// Rutas del usuario
+router.get("/users", userController.index, userController.indexView);
+router.get("/users/new", userController.newUser);
+router.post(
+  "/users/create",
+  userController.create,
+  userController.redirectView
+);
+router.get("/users/get/:id", userController.show, userController.showView);
+router.get("/users/:id/edit", userController.edit);
+router.put(
+  "/users/:id/update",
+  userController.update,
+  userController.redirectView
+);
+router.delete(
+  "/users/:id/delete",
+  userController.erased,
+  userController.redirectView
+);
+
+// Rutas de los cursos
+router.get("/courses", courseController.index, courseController.indexView);
+router.get("/courses/new", courseController.newCourse);
+router.post(
+  "/courses/create",
+  courseController.create,
+  courseController.redirectView
+);
+router.get(
+  "/courses/get/:id",
+  courseController.show,
+  courseController.showView
+);
+router.get("/courses/:id/edit", courseController.edit);
+router.put(
+  "/courses/:id/update",
+  courseController.update,
+  courseController.redirectView
+);
+router.delete(
+  "/courses/:id/delete",
+  courseController.erased,
+  courseController.redirectView
+);
 
 // Middleware para manejar los errores
 app.use(errorController.pageNotFoundError);
